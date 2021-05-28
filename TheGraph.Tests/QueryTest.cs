ae5464c38399f7323d1ab7c58c01e25f25d46cd2
michielpost <michiel@q42.nl>
2021-05-28 17:24:17 +0200

    Basic query

--- TheGraph.Tests/QueryTest.cs
+++ TheGraph.Tests/QueryTest.cs
@@ -1,4 +1,5 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading.Tasks;
 
 namespace TheGraph.Tests
 {
@@ -6,8 +7,24 @@ namespace TheGraph.Tests
     public class QueryTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public async Task TestMethod1()
         {
+            var client = new TheGraphClient("https://api.thegraph.com/subgraphs/name/michielpost/graph-bookstore");
+
+            string query = @"
+                        {
+                          gravatars(first: 5) {
+                            id
+                            owner
+                            displayName
+                            imageUrl
+                          }
+                        }
+                        ";
+
+            var result = await client.SendQueryAsync<dynamic>(query);
+
+            Assert.IsTrue(result.Errors == null);
         }
     }
 }
--- TheGraph/TheGraphClient.cs
+++ TheGraph/TheGraphClient.cs
@@ -1,8 +1,32 @@
-﻿using System;
+﻿using GraphQL;
+using GraphQL.Client.Http;
+using GraphQL.Client.Serializer.SystemTextJson;
+using GraphQL.Client.Abstractions;
+using System;
+using System.Threading.Tasks;
 
 namespace TheGraph
 {
     public class TheGraphClient
     {
+        private readonly GraphQLHttpClient client;
+
+        public TheGraphClient(string baseUrl)
+        {
+            client = new GraphQLHttpClient(baseUrl, new SystemTextJsonSerializer());
+
+        }
+
+        public async Task<GraphQLResponse<T>> SendQueryAsync<T>(string query)
+        {
+            var request = new GraphQLRequest
+            {
+                Query = query
+            };
+
+            var graphQLResponse = await client.SendQueryAsync<T>(request);
+
+            return graphQLResponse;
+        }
     }
 }
